781ec12a8bd02001d4867a3b80683642a3be5b93
safayetjamil647 <work.jamil4@gmail.com>
2022-09-21 17:14:24 +0600

    basic cli

---
+++ cmd/root.go
@@ -0,0 +1,26 @@
+package cmd
+
+import (
+    "context"
+    // "errors"
+
+    "github.com/spf13/cobra"
+    // "github.com/spf13/viper"
+    // "golang.org/x/sync/errgroup"
+)
+
+// Execute is the command line applications entry function
+func Execute() error {
+    rootCmd := &cobra.Command{
+        Version: "v0.0.1",
+        Use:     "xor",
+		Long:    "xor for your devops environment",
+        Example: "xor",
+        RunE: func(cmd *cobra.Command, args []string) error {
+            return nil
+        },
+    }
+
+    return rootCmd.ExecuteContext(context.Background())
+}
+
---
+++ main.go
@@ -0,0 +1,11 @@
+/*
+Copyright © 2022 SAFAYET JAMIL <kazisafayetjamil@gmail.com>
+
+*/
+package main
+
+import "github.com/safayetjamil647/terminal-xor/cmd"
+
+func main() {
+	cmd.Execute()
+}
